d280c2c6e3c8a16e1c36945c4050575b81ac28f2
shanmingyuan <mshan@u.rochester.edu>
2021-12-11 14:04:25 -0800

    added test for server package except never ending thread

--- assignment6/src/test/java/Server/ChatroomServerThreadTest.java
+++ assignment6/src/test/java/Server/ChatroomServerThreadTest.java
@@ -1,25 +1,75 @@
 package Server;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ConcurrentHashMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ChatroomServerThreadTest {
 
+  private Socket socket = mock(Socket.class);
+  private ChatroomServerThread thread;
+  private ConcurrentHashMap<String, DataOutputStream> sharedBuffers = new ConcurrentHashMap<>();
+
   @BeforeEach
   void setUp() throws IOException {
+    thread = new ChatroomServerThread(sharedBuffers, socket);
+    ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+    DataOutputStream out = new DataOutputStream(bOut);
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bOut.toByteArray()));
+    when(socket.getInputStream()).thenReturn(in);
+    ByteArrayOutputStream bOuter = new ByteArrayOutputStream();
+    when(socket.getOutputStream()).thenReturn(new DataOutputStream(bOuter));
   }
 
   @Test
   void testEquals() throws IOException {
+    ChatroomServerThread myThread = new ChatroomServerThread(sharedBuffers, socket);
+    ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+    DataOutputStream out = new DataOutputStream(bOut);
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bie".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bie".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bOut.toByteArray()));
+    when(socket.getInputStream()).thenReturn(in);
+    ByteArrayOutputStream bOuter = new ByteArrayOutputStream();
+    when(socket.getOutputStream()).thenReturn(new DataOutputStream(bOuter));
 
+    assertEquals(thread, thread);
+    assertNotEquals(thread, myThread);
+    assertNotEquals(thread, null);
+    assertNotEquals(thread, 1);
   }
 
   @Test
   void testHashCode() {
+    assertEquals(thread.hashCode(), thread.hashCode());
   }
+
 }
--- assignment6/src/test/java/Server/ServerChatroomProtocolTest.java
+++ assignment6/src/test/java/Server/ServerChatroomProtocolTest.java
@@ -1,9 +1,8 @@
 package Server;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import Client.ClientChatroomProtocol;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -15,10 +14,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ServerChatroomProtocolTest {
+
   private ServerChatroomProtocol protocol;
   private ConcurrentHashMap<String, DataOutputStream> sharedBuffers = new ConcurrentHashMap<>();
-  private DataInputStream in = mock(DataInputStream.class);
-  private DataOutputStream out = mock(DataOutputStream.class);
+  private ByteArrayOutputStream bo = new ByteArrayOutputStream();
+  private DataOutputStream out = new DataOutputStream(bo);
 
   @BeforeEach
   void setUp() throws IOException {
@@ -26,19 +26,326 @@ class ServerChatroomProtocolTest {
 
   @Test
   void serverProcessConnectMessage() throws IOException {
-    when(in.readInt()).thenReturn(19).thenReturn(3);
-    when(in.readChar()).thenReturn(' ');
-    when(in.read(any(byte[].class))).thenReturn(3);
-
-    this.protocol = new ServerChatroomProtocol(in, out, sharedBuffers);
-
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
   }
 
   @Test
-  void testEquals() {
+  void serverProcessConnectMessageWithDuplicateUserName() throws IOException {
+    sharedBuffers.put("Bob", new DataOutputStream(new ByteArrayOutputStream()));
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
   }
 
   @Test
-  void testHashCode() {
+  void serverProcessDisconnectMessage() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
+  }
+
+  @Test
+  void serverProcessQueryUsers() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(22);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
+  }
+
+  @Test
+  void serverProcessDirectMessage() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(25);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    String message = "Hello me";
+    out.writeChar(' ');
+    out.writeInt(message.getBytes(StandardCharsets.UTF_8).length);
+    out.writeChar(' ');
+    out.write(message.getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
+  }
+
+  @Test
+  void serverProcessDirectMessageWithIncorrectUserName() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(25);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeChar(' ');
+    out.writeInt(4);
+    out.writeChar(' ');
+    out.write("Bobb".getBytes(StandardCharsets.UTF_8));
+    String message = "Hello me";
+    out.writeChar(' ');
+
+    out.writeInt(message.getBytes(StandardCharsets.UTF_8).length);
+    out.writeChar(' ');
+    out.write(message.getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
+  }
+
+  @Test
+  void serverProcessBroadcastMessage() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(24);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    String message = "Hello me";
+    out.writeChar(' ');
+    out.writeInt(message.getBytes(StandardCharsets.UTF_8).length);
+    out.writeChar(' ');
+    out.write(message.getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
+  }
+
+  @Test
+  void serverProcessSendInsult() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(27);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    protocol.serverProcess();
+    DataInputStream nowWeRead = new DataInputStream(
+        new ByteArrayInputStream(outputTestArray.toByteArray()));
+    assertEquals(nowWeRead.readInt(), 20);
+    assertEquals(nowWeRead.readChar(), ' ');
+  }
+
+  @Test
+  void testEquals() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Ann".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Ann".getBytes(StandardCharsets.UTF_8));
+    DataInputStream inn = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArrayy = new ByteArrayOutputStream();
+    DataOutputStream outputTestt = new DataOutputStream(outputTestArrayy);
+    ServerChatroomProtocol protocoll = new ServerChatroomProtocol(inn, outputTestt, sharedBuffers);
+
+    assertEquals(protocol, protocol);
+    assertNotEquals(protocol, null);
+    assertNotEquals(protocol, 1);
+    assertNotEquals(protocol, protocoll);
+  }
+
+  @Test
+  void testEqualsAnother() throws Exception {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream innn = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArrayyy = new ByteArrayOutputStream();
+    DataOutputStream outputTesttt = new DataOutputStream(outputTestArrayyy);
+    ServerChatroomProtocol protocolll = new ServerChatroomProtocol(innn, outputTesttt,
+        new ConcurrentHashMap<>());
+    assertNotEquals(protocol, protocolll);
+  }
+
+  @Test
+  void testHashCode() throws IOException {
+    out.writeInt(19);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    out.writeInt(21);
+    out.writeChar(' ');
+    out.writeInt(3);
+    out.writeChar(' ');
+    out.write("Bob".getBytes(StandardCharsets.UTF_8));
+    DataInputStream in = new DataInputStream(new ByteArrayInputStream(bo.toByteArray()));
+    ByteArrayOutputStream outputTestArray = new ByteArrayOutputStream();
+    DataOutputStream outputTest = new DataOutputStream(outputTestArray);
+    protocol = new ServerChatroomProtocol(in, outputTest, sharedBuffers);
+    assertEquals(protocol.hashCode(), protocol.hashCode());
   }
 }
